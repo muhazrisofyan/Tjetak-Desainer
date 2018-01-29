f451d0091a5645dbe803ef359741c482305e807c
hizkian <hiznugroho@gmail.com>
2018-01-29 23:37:13 +0700

    tjetak landing page quarter done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,16 +5,16 @@
     <title>Tjetak Designer</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="/css/style.css">
+
   </head>
   <body>
     {{-- Header --}}
-    <div class="w3-top">
-      <img class="w3-padding" width="250px" src="{{url('img/b_tentang_tjetak_desainer/Logo/logo_tjetak_desainer_x1.png')}}" alt="">
+    <div id="header" class="w3-top w3-bar w3-white w3-padding">
+      <img class="w3-padding w3-hover-opacity" src="{{url('img/b_tentang_tjetak_desainer/Logo/logo_tjetak_desainer_x1.png')}}" alt="">
         <!-- Float links to the right. Hide them on small screens -->
         <div class="w3-right w3-hide-small">
-          <a href="#projects" class="w3-bar-item w3-button">Projects</a>
-          <a href="#about" class="w3-bar-item w3-button">About</a>
-          <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+          <a class="w3-btn w3-round w3-border button" href="/login">Masuk Sebagai Desainer</a>
+          <a class="w3-btn w3-round w3-blue w3-border button" href="/register">Daftar Sebagai Desainer</a>
         </div>
       </div>
     </div>
--- resources/views/pages/landingpage.blade.php
+++ resources/views/pages/landingpage.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.master')
 
 @section('content')
+  {{-- landingpage banner --}}
+  <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
+    <div class="w3-display-middle" style="white-space:nowrap;">
+
+    </div>
+  </div>
+  {{-- End of Banner --}}
+
+  <div class="w3-container w3-display-container w3-row w3-round w3-white" style="margin:3%">
+    <div class="w3-threequarter">
+      <h4>Tentang</h4>
+      <h4>TJETAK DESAINER</h4>
+      <p style="text-align:justify">Tjetak Designer adalah platform dimana Designer dapat bertemu dengan
+        mereka yang membutuhkan jasa desain. Di sini, para desainer dapat bekerja
+        secara online untuk memenuhi kebutuhan design yang ada dengan kemudahaan
+        bekerja dimanapun.</p>
+    </div>
+    <div class="w3-quarter">
+      <img class="w3-display-right w3-margin-right" src="{{url('img/b_tentang_tjetak_desainer/Logo/logo_tjetak_desainer_x1.png')}}" style="width:220px" alt="">
+    </div>
+  </div>
 
 @endsection
