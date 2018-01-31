86a730539b20c34d4294adfd8fc74f2306f72d57
hizkian <hiznugroho@gmail.com>
2018-01-31 23:50:18 +0700

    bar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,22 +2,38 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tjetak Designer</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/style.css">
 
   </head>
   <body>
     {{-- Header --}}
-    <div id="header" class="w3-top w3-bar w3-white w3-padding w3-border">
+    <div id="header" class="w3-top w3-bar w3-white w3-padding-8 w3-border">
       <img class="w3-padding w3-hover-opacity" src="{{asset('storage/img/b_tentang_tjetak_desainer/Logo/logo_tjetak_desainer_x1.png')}}" alt="">
         <!-- Float links to the right. Hide them on small screens -->
-        <div class="w3-right w3-hide-small">
-          <a class="w3-btn w3-round w3-border button w3-small" href="/login">Masuk Sebagai Desainer</a>
-          <a class="w3-btn w3-round w3-blue w3-border button w3-small" href="/register">Daftar Sebagai Desainer</a>
+        <div class="w3-right w3-hide-medium w3-hide-small">
+          <a class="w3-btn w3-round w3-border c-button w3-small" href="/login">Masuk Sebagai Desainer</a>
+          <a class="w3-btn w3-round w3-blue w3-border c-button w3-small" href="/register">Daftar Sebagai Desainer</a>
         </div>
+
+        {{-- Mobile Dropdown --}}
+        <button onclick="myFunction('Demo1')" class="w3-btn w3-right w3-hide-large"><i id="bar" class="w3-xlarge fa fa-bars" aria-hidden="true"></i></button>
+
+        <div id="Demo1" class="w3-hide w3-margin-top ssp-regular w3-padding w3-hide-large">
+          <a href="">
+            <button class="width-100 w3-btn w3-round w3-border w3-small" type="button" name="button">Masuk Sebagai Desainer</button>
+          </a>
+          <a href="">
+            <button class="width-100 w3-margin-top w3-btn w3-round w3-blue w3-border w3-small" type="button" name="button">Daftar Sebagai Desainer</button>
+          </a>
+        </div>
+        {{-- End of Mobile Dropdown --}}
       </div>
     </div>
+
     {{-- End of Header --}}
 
     {{-- Content --}}
@@ -35,5 +51,23 @@
       </div>
     </div>
     {{-- End of Footer --}}
+
+    {{-- Header and footer script --}}
+    <script>
+    function myFunction(id) {
+        var x = document.getElementById(id);
+        var bar = document.getElementById("bar");
+        if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+            bar.classList.remove('fa-bars');
+            bar.classList.add('fa-times');
+        } else {
+            x.className = x.className.replace(" w3-show", "");
+            bar.classList.remove('fa-times');
+            bar.classList.add('fa-bars');
+        }
+    }
+    </script>
+    {{-- End of Script --}}
   </body>
 </html>
--- resources/views/pages/landingpage.blade.php
+++ resources/views/pages/landingpage.blade.php
@@ -3,23 +3,27 @@
 @section('content')
   {{-- landingpage banner --}}
   <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
-    <div class="w3-display-middle" style="white-space:nowrap;">
-
+    <div class="w3-container w3-display-left w3-row" style="">
+      <div class="ssp-bold w3-col m12 l10 w3-text-white mobile-center">
+        <h1 class="w3-xxlarge w3-animate-left little-spacing-after">Desain dan dapatkan pemasukan tambahan.</h1>
+        <p class="w3-large">Rasakan manfaat menjadi desainer produk percetakan<br> kami dengan pemasukan tambahan
+        yang kompetitif.</p>
+      </div>
     </div>
   </div>
   {{-- End of Banner --}}
 
   {{-- Tentang Tjetak Desainer --}}
   <div class="w3-border w3-container w3-display-container w3-round w3-row w3-white w3-responsive w3-card-4" style="margin:3%">
-    <div class="w3-threequarter">
+    <div class="w3-col m7 l9">
       <h4 class="ssp-regular w3-text-grey">Tentang</h4>
-      <h1 class="ssp-bold w3-text-orange text-bold margin-top-min20">TJETAK DESAINER</h1>
+      <h1 class="ssp-bold w3-text-orange c-text-bold margin-top-min20">TJETAK DESAINER</h1>
       <p class="ssp-regular text-justify">Tjetak Designer adalah platform dimana Designer dapat bertemu dengan
         mereka yang membutuhkan jasa desain. Di sini, para desainer dapat bekerja
         secara online untuk memenuhi kebutuhan design yang ada dengan kemudahaan
         bekerja dimanapun.</p>
     </div>
-    <div class="w3-rest" style="width:20%">
+    <div class="w3-col m4 l3 w3-hide-small">
       <img class="w3-display-right w3-margin-right" src="{{asset('storage/img/b_tentang_tjetak_desainer/Logo/logo_tjetak_desainer_x1.png')}}" style="width:220px" alt="logo tjetak desainer">
     </div>
   </div>
@@ -28,7 +32,7 @@
   {{-- Cara Kerja --}}
   <div class="w3-border w3-container w3-display-container w3-round w3-row w3-white w3-responsive w3-card-4" style="margin:3%">
     <h4 class="ssp-regular w3-text-grey">Cara Kerja</h4>
-    <h1 class="ssp-bold w3-text-orange text-bold margin-top-min20">TJETAK DESAINER</h1>
+    <h1 class="ssp-bold w3-text-orange c-text-bold margin-top-min20">TJETAK DESAINER</h1>
     <div class="ssp-regular w3-row">
       <div class="w3-center w3-third mobile-center">
         <img src={{asset("storage/img/c_cara_kerja/icon_cara_kerja_1.svg")}} alt="icon cara kerja 1">
@@ -49,14 +53,14 @@
   {{-- Produk --}}
   <div class="w3-border w3-container w3-round w3-row w3-white w3-responsive w3-card-4" style="margin:3%">
     <h4 class="ssp-regular w3-text-grey">Desain untuk</h4>
-    <h1 class="ssp-bold w3-text-orange text-bold margin-top-min20">PRODUK-PRODUK</h1>
+    <h1 class="ssp-bold w3-text-orange c-text-bold margin-top-min20">PRODUK-PRODUK</h1>
     <div class="ssp-regular w3-row">
       @foreach ($files as $file)
         <div class="w3-third w3-padding ssp-regular">
           <a class="a-decoration-none" href="">
             <div class="w3-container w3-card-2 w3-round-large width-100 w3-margin-bottom">
               <img class="w3-image responsive w3-circle w3-left w3-margin" src={{asset("storage/img/d_produk/") . '/' . explode('/', $file)[3]}} alt="Gambar amplop">
-              <p class="text-bold product-text-middle">{{str_replace("-", " ", substr(explode('/', $file)[3], 0, strlen(explode('/', $file)[3]) - 4))}}</p>
+              <p class="c-text-bold product-text-middle">{{str_replace("-", " ", substr(explode('/', $file)[3], 0, strlen(explode('/', $file)[3]) - 4))}}</p>
             </div>
           </a>
         </div>
@@ -68,7 +72,7 @@
   {{-- Keuntungan --}}
   <div class="w3-border w3-container w3-round w3-row w3-white w3-card-4" style="margin:3%">
     <h4 class="ssp-regular w3-text-grey">Keuntungan</h4>
-    <h1 class="ssp-bold w3-text-orange text-bold margin-top-min20">TJETAK DESAINER</h1>
+    <h1 class="ssp-bold w3-text-orange c-text-bold margin-top-min20">TJETAK DESAINER</h1>
     <div class="w3-row width-100 w3-padding">
       <div class="w3-col m4 l3 w3-center">
         <img src={{asset("storage/img/c_cara_kerja/icon_cara_kerja_1.svg")}}>
