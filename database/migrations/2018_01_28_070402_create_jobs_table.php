137ae86c4fc7db2bea62c479bb6a374401b447c2
muhazrisofyan <muhazrisofyan@gmail.com>
2018-02-09 00:59:00 +0700

    finish week1

--- database/migrations/2018_01_28_070402_create_jobs_table.php
+++ database/migrations/2018_01_28_070402_create_jobs_table.php
@@ -11,26 +11,26 @@ class CreateJobsTable extends Migration
      *
      * @return void
      */
-    // public function up()
-    // {
-    //     Schema::create('jobs', function (Blueprint $table) {
-    //         $table->bigIncrements('id');
-    //         $table->string('queue')->index();
-    //         $table->longText('payload');
-    //         $table->unsignedTinyInteger('attempts');
-    //         $table->unsignedInteger('reserved_at')->nullable();
-    //         $table->unsignedInteger('available_at');
-    //         $table->unsignedInteger('created_at');
-    //     });
-    // }
-    //
-    // /**
-    //  * Reverse the migrations.
-    //  *
-    //  * @return void
-    //  */
-    // public function down()
-    // {
-    //     Schema::dropIfExists('jobs');
-    // }
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
 }
