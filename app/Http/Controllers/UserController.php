39f9a35aa9612952f0303ac90ecb002978e86423
muhazrisofyan <muhazrisofyan@gmail.com>
2018-03-03 17:58:20 +0700

    edit controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,4 +45,86 @@ class UserController extends Controller
 
     return redirect()->back()->with("success","Password changed successfully !");
   }
+
+  /**
+
+  * Handle a registration request for the application.
+
+  *
+
+  * @param \Illuminate\Http\Request $request
+
+  * @return \Illuminate\Http\Response
+
+  */
+
+  public function changeEmail(Request $request)
+
+  {
+
+    $this->validator($request->all())->validate();
+    event(new Registered($user = $this->create($request->all())));
+    dispatch(new SendVerificationEmail($user));
+    return view('auth.verification');
+
+  }
+
+  /**
+
+  * Handle a registration request for the application.
+
+  *
+
+  * @param $token
+
+  * @return \Illuminate\Http\Response
+
+  */
+
+  public function verify($token)
+
+  {
+
+    $user = User::where('email_token',$token)->first();
+    $user->verified = 1;
+    if($user->save()){
+      return view('auth.emailConfirmation',['user'=>$user]);
+    }
+
+  }
+
+  /**
+   * Get a validator for an incoming registration request.
+   *
+   * @param  array  $data
+   * @return \Illuminate\Contracts\Validation\Validator
+   */
+  protected function validator(array $data)
+  {
+      // dd($data);
+      return Validator::make($data, [
+          'name' => 'required|string|max:255',
+          'email' => 'required|string|email|max:255|unique:users',
+          'phone' => 'required|string|max:14|regex:/[0-9]*/',
+          'password' => 'required|string|min:6|confirmed'
+      ]);
+  }
+
+  /**
+   * Create a new user instance after a valid registration.
+   *
+   * @param  array  $data
+   * @return \App\User
+   */
+  protected function create(array $data)
+  {
+    // dd($data);
+      return User::create([
+          'name' => $data['name'],
+          'email' => $data['email'],
+          'password' => bcrypt($data['password']),
+          'email_token' => base64_encode($data['email']),
+          'phone' => $data['phone'],
+      ]);
+  }
 }
